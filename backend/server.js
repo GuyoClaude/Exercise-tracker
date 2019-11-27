fb62ee799e46384f4952f97d122656545573623c
claude009 <37622842+claude009@users.noreply.github.com>
2019-11-27 19:38:02 +0300

    Another

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -27,8 +26,6 @@ const usersRouter = require("./routes/users");
 
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
